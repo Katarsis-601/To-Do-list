7eaf8109d48b1ab489f732fa77946bd97d4f06a0
Andriano <64583684+Andrian-W@users.noreply.github.com>
2022-04-13 23:14:33 +0700

    Adding delete features and more activity

--- public/app.js
+++ public/app.js
@@ -1,24 +1,114 @@
 const root = document.querySelector("main");
 
 function App() {
+  const [activity, setActivity] = React.useState("");
+  const [activity2, setActivity2] = React.useState("");
+  const [activity3, setActivity3] = React.useState("");
+  const [activity4, setActivity4] = React.useState("");
+  const [activity5, setActivity5] = React.useState("");
+  const [date, setDate] = React.useState("");
+  const [todo, setTodo] = React.useState([]);
+
+  function deleteData(id) {
+    const dataAfterFilter = todo.filter(data => {
+      return data.id != id;
+    });
+    setTodo(dataAfterFilter);
+  }
+
+  function addData(event) {
+    event.preventDefault();
+    let data = {
+      id: Date.now(),
+      activity,
+      activity2,
+      activity3,
+      activity4,
+      activity5,
+      date
+    };
+    setTodo([...todo, data]);
+    setActivity("");
+    setActivity2("");
+    setActivity3("");
+    setActivity4("");
+    setActivity5("");
+  }
+
+  React.useEffect(() => console.log(todo), [todo]);
   return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("section", {
     className: "content"
+  }, /*#__PURE__*/React.createElement("h1", null, "To Do List"), /*#__PURE__*/React.createElement("form", {
+    className: "activity-contain",
+    onSubmit: addData
   }, /*#__PURE__*/React.createElement("div", {
-    className: "activity-contain"
+    className: "inputActivity-contain"
   }, /*#__PURE__*/React.createElement("input", {
     type: "text",
     id: "activity",
     name: "activity",
-    placeholder: "Input your Activity here"
+    placeholder: "Input your Activity here",
+    onChange: ev => setActivity(ev.target.value),
+    value: activity,
+    required: true
   }), /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    id: "activity",
+    name: "activity",
+    placeholder: "Input your Activity here",
+    onChange: ev => setActivity2(ev.target.value),
+    value: activity2
+  }), /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    id: "activity",
+    name: "activity",
+    placeholder: "Input your Activity here",
+    onChange: ev => setActivity3(ev.target.value),
+    value: activity3
+  }), /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    id: "activity",
+    name: "activity",
+    placeholder: "Input your Activity here",
+    onChange: ev => setActivity4(ev.target.value),
+    value: activity4
+  }), /*#__PURE__*/React.createElement("input", {
+    type: "text",
+    id: "activity",
+    name: "activity",
+    placeholder: "Input your Activity here",
+    onChange: ev => setActivity5(ev.target.value),
+    value: activity5
+  })), /*#__PURE__*/React.createElement("p", null, "Date Activity : "), /*#__PURE__*/React.createElement("input", {
     type: "date",
     id: "date_act",
     name: "date_act",
-    placeholder: "Date"
+    value: date,
+    onChange: ev => setDate(ev.target.value),
+    required: true
   }), /*#__PURE__*/React.createElement("button", {
-    type: "button",
+    type: "submit",
     className: "add-btn"
-  }, "Add Activity"))));
+  }, "Add Activity")), /*#__PURE__*/React.createElement("ul", null, todo.map((data, i) => /*#__PURE__*/React.createElement("li", {
+    key: i
+  }, /*#__PURE__*/React.createElement("p", {
+    className: "date_act"
+  }, data.date), /*#__PURE__*/React.createElement("p", {
+    className: "activity"
+  }, data.activity), /*#__PURE__*/React.createElement("p", {
+    className: "activity"
+  }, data.activity2), /*#__PURE__*/React.createElement("p", {
+    className: "activity"
+  }, data.activity3), /*#__PURE__*/React.createElement("p", {
+    className: "activity"
+  }, data.activity4), /*#__PURE__*/React.createElement("p", {
+    className: "activity"
+  }, data.activity5), /*#__PURE__*/React.createElement("button", {
+    className: "delete",
+    onClick: () => {
+      deleteData(data.id);
+    }
+  }, "Delete"))))));
 }
 
 ReactDOM.render( /*#__PURE__*/React.createElement(App, null), root);
--- src/app.js
+++ src/app.js
@@ -1,21 +1,125 @@
 const root = document.querySelector("main");
 
 function App() {
+  const [activity, setActivity] = React.useState("");
+  const [activity2, setActivity2] = React.useState("");
+  const [activity3, setActivity3] = React.useState("");
+  const [activity4, setActivity4] = React.useState("");
+  const [activity5, setActivity5] = React.useState("");
+
+  const [date, setDate] = React.useState("");
+  const [todo, setTodo] = React.useState([]);
+
+  function deleteData(id) {
+    const dataAfterFilter = todo.filter((data) => {
+      return data.id != id;
+    });
+    setTodo(dataAfterFilter);
+  }
+
+  function addData(event) {
+    event.preventDefault();
+
+    let data = {
+      id: Date.now(),
+      activity,
+      activity2,
+      activity3,
+      activity4,
+      activity5,
+      date,
+    };
+
+    setTodo([...todo, data]);
+    setActivity("");
+    setActivity2("");
+    setActivity3("");
+    setActivity4("");
+    setActivity5("");
+  }
+  React.useEffect(() => console.log(todo), [todo]);
   return (
     <>
       <section className="content">
-        <div className="activity-contain">
+        <h1>To Do List</h1>
+        <form className="activity-contain" onSubmit={addData}>
+          <div className="inputActivity-contain">
+            <input
+              type="text"
+              id="activity"
+              name="activity"
+              placeholder="Input your Activity here"
+              onChange={(ev) => setActivity(ev.target.value)}
+              value={activity}
+              required
+            />
+            <input
+              type="text"
+              id="activity"
+              name="activity"
+              placeholder="Input your Activity here"
+              onChange={(ev) => setActivity2(ev.target.value)}
+              value={activity2}
+            />
+            <input
+              type="text"
+              id="activity"
+              name="activity"
+              placeholder="Input your Activity here"
+              onChange={(ev) => setActivity3(ev.target.value)}
+              value={activity3}
+            />
+            <input
+              type="text"
+              id="activity"
+              name="activity"
+              placeholder="Input your Activity here"
+              onChange={(ev) => setActivity4(ev.target.value)}
+              value={activity4}
+            />
+            <input
+              type="text"
+              id="activity"
+              name="activity"
+              placeholder="Input your Activity here"
+              onChange={(ev) => setActivity5(ev.target.value)}
+              value={activity5}
+            />
+          </div>
+
+          <p>Date Activity : </p>
           <input
-            type="text"
-            id="activity"
-            name="activity"
-            placeholder="Input your Activity here"
+            type="date"
+            id="date_act"
+            name="date_act"
+            value={date}
+            onChange={(ev) => setDate(ev.target.value)}
+            required
           />
-          <input type="date" id="date_act" name="date_act" placeholder="Date" />
-          <button type="button" className="add-btn">
+          <button type="submit" className="add-btn">
             Add Activity
           </button>
-        </div>
+        </form>
+        <ul>
+          {todo.map((data, i) => (
+            <li key={i}>
+              <p className="date_act">{data.date}</p>
+              <p className="activity">{data.activity}</p>
+              <p className="activity">{data.activity2}</p>
+              <p className="activity">{data.activity3}</p>
+              <p className="activity">{data.activity4}</p>
+              <p className="activity">{data.activity5}</p>
+              <button
+                className="delete"
+                onClick={() => {
+                  deleteData(data.id);
+                }}
+              >
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
       </section>
     </>
   );
